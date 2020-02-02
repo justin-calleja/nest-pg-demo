a61236455fefb11ec147bf2ed9b285f094aab52f
Justin Calleja <justin.calleja@mrggroup.com>
2020-02-02 20:03:03 +0100

    Add constants and define provider in db module

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { Pool } from 'pg';
+import { PG_CONNECTION } from '../constants';
 
-@Module({})
+const dbProvider = {
+  provide: PG_CONNECTION,
+  useValue: new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'somedb',
+    password: 'meh',
+    port: 5432,
+  }),
+};
+
+@Module({
+  providers: [dbProvider],
+  exports: [dbProvider],
+})
 export class DbModule {}
