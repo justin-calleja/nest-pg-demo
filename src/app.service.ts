effc394b78d428b6ffb127692e3f7aacdbee0a61
Justin Calleja <justin.calleja@mrggroup.com>
2020-02-02 20:03:20 +0100

    Use the provider

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { PG_CONNECTION } from './constants';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(PG_CONNECTION) private conn: any) {}
+
+  async getUsers() {
+    const res = await this.conn.query('SELECT * FROM users');
+    return res.rows;
   }
 }
